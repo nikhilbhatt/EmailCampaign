a14e69411d6e77b40ffeef06c6168e63f39b20ac
nikhilbhatt <nikhilbhatt931@gmail.com>
2020-03-19 22:59:51 +0530

    view updated

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -3,7 +3,7 @@
     <div class="col-md-4" style="margin-top:250px;">
       <div style="margin-left:25px;">
           <h1 style="color:blue; font-family:impact; font-style:bold; font-size:83px">About Us</h1>
-          <p style="font-family:Microsoft Yi Baiti; font-size:30px;">We are the small startup company focussed upon problem solving.</p>
+          <p style="font-family:Microsoft Yi Baiti; font-size:30px;">We provide free of cost email servicing.</p>
           
       </div>
     </div>
@@ -11,5 +11,20 @@
        background-repeat:no-repeat;">
     </div>
 </div>
-<?php require_once APPROOT.'/views/includes/bottom.php'?>
+<div>
+    <footer class="page-footer font-small navbar-dark bg-light pt-2 " style="position:relative; 
+    left:0;
+    width:100%;
+    bottom: 0;
+    font-family: 'Cantarell';
+    font-size: 16px;">
+    <hr class="mt-0" style="color:#123455; height:1px; background-color: #123455; border:none;">
+          <div class="text-center">
+          <p class="text-dark">Email Campaign Webapp developed during 1 month internship</p>
+          </div>
+      <div class="footer-copyright text-center py-1">© 2020 Copyright:
+        <a href="https://coloredcow.com"> ColoredCow</a>
+      </div>
+    </footer>
+</div>
 <?php require_once APPROOT.'/views/includes/footer.php' ?>
